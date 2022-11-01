c4877bca7f9c14a4d131a64cb1cbc289807a0918
Erickson Lima <ericksonlbs@outlook.com>
2022-11-01 01:24:51 -0300

    Fixed prepare.sh to build jaguar-maven-plugin

--- .github/scripts/prepare.sh
+++ .github/scripts/prepare.sh
@@ -2,13 +2,17 @@ git clone https://github.com/SIN5005-EngSoft2021/jaguar
 
 cd jaguar
 
-./mvnw install:install-file -Dfile=br.usp.each.saeg.jaguar.core/lib/org.jacoco.core-0.7.6.jar \
+mvn install:install-file -Dfile=br.usp.each.saeg.jaguar.core/lib/org.jacoco.core-0.7.6.jar \
      -DgroupId=br.usp.each.saeg -DartifactId=org.jacoco.core \
      -Dversion=0.7.6 -Dpackaging=jar
-     
-./mvnw clean install -pl br.usp.each.saeg.jaguar.maven.plugin -am
 
-./mvnw org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file \
+mvn install -pl br.usp.each.saeg.jaguar.core -am
+
+cp ./br.usp.each.saeg.jaguar.core/target/br.usp.each.saeg.jaguar.core-1.0.0-jar-with-dependencies.jar ./br.usp.each.saeg.jaguar.maven.plugin/src/main/lib/jaguar.core-1.0.0-jar-with-dependencies.jar
+     
+mvn clean install -pl br.usp.each.saeg.jaguar.maven.plugin -am
+
+mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file \
      -Dfile=./br.usp.each.saeg.jaguar.maven.plugin/target/jaguar-maven-plugin-0.0.1-SNAPSHOT.jar
 
 cd .. 
